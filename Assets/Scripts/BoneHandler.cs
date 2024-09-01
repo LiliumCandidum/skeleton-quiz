92edbfcd622ee0a3b5a048de3759170ecc756a14
Paola Nicosia <paoletta12.n@gmail.com>
2024-09-01 18:26:29 +0200

    wip

---
+++ Assets/Scripts/BoneHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneHandler : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Debug.Log("ON MOUSE DOWN");
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+}
