a17421d0c6a844631e95ceb0b97b632453756ca0
Alessio <>
2024-09-29 13:47:08 +0200

    - Aggiunto gamemanager, piu pulito - Ridimensionato canvas e camera - Creato sottogruppi per ossa di piedi e mani - Aggiunto spawn di gruppi di ossa - Aggiunto asset QuickOutline per evidenziare singolo osso - Gruppi di ossa centrati - Aggiunto primo tentativo di rotazione con mouse

--- Assets/Scripts/DynamicBone.cs
+++ Assets/Scripts/DynamicBone.cs
@@ -1,52 +1,152 @@
-using System.Collections;
-using System.Collections.Generic;
+
+
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class DynamicBone : MonoBehaviour
 {
-    public GameObject[] objectsToChooseFrom;
-    public Transform displayPoint;
-    private GameObject currentObject;
 
-    private string[] prefabNames = {"FJ3201_BP22977_FMA52749_Hyoid bone", "FJ3392_BP23110_FMA52892_Right zygomatic bone", "FJ6471_BP24091_FMA53655_Right palatine bone"};
+    private GameObject currentGroup;
+
+    private string[] MACRO_GROUPS = { "Skull", "Body", "LeftArm", "RightArm", "LeftLeg", "RightLeg" };    // Five macro groups of bones: Skull, Body, LeftArm, RightArm, LeftLeg, RightLeg
+    private string[] SUB_GROUPS = { "Vertebra", "Rib", "LeftHand", "RightHand", "LeftFoot", "RightFoot" };    // Five macro groups of bones: Skull, Body, LeftArm, RightArm, LeftLeg, RightLeg
+    [SerializeField] private GameObject[] prefabGroups;
+    [SerializeField] private Transform displayPoint;
 
     void Start()
     {
-        objectsToChooseFrom = new GameObject[prefabNames.Length];
+        NextBone();
 
-        for(int i=0; i < prefabNames.Length; i++)
-        {
-            objectsToChooseFrom[i] = GameObject.Find(prefabNames[i]);
-        }
     }
+
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
-            if (currentObject != null)
+            NextBone();
+
+        }
+    }
+
+    private void NextBone()
+    {
+        if (currentGroup != null)
+        {
+            Destroy(currentGroup);
+        }
+
+        int layerUI = LayerMask.NameToLayer("UI");
+
+        // pick one of the five macrogroups
+        int groupIndex = UnityEngine.Random.Range(0, prefabGroups.Length);
+
+        if (prefabGroups[groupIndex] != null)
+        {
+            GameObject group = prefabGroups[groupIndex];
+            Debug.Log(group.name);
+
+            // pick a bone or a subgroup of bones inside the macrogroup 
+            int boneIndex = UnityEngine.Random.Range(0, group.transform.childCount);
+            GameObject bone = group.transform.GetChild(boneIndex).gameObject;
+            Debug.Log(bone.name);
+
+            // if is a subgroup, we must pick again another bone inside the subgroup
+            if (SUB_GROUPS.Contains(bone.name))
             {
-                Destroy(currentObject);
+                group = bone;
+                boneIndex = UnityEngine.Random.Range(0, group.transform.childCount);
+                bone = group.transform.GetChild(boneIndex).gameObject;
+                Debug.Log(bone.name);
             }
 
-            int layerUI = LayerMask.NameToLayer("UI");
-            int randomNumber = Random.Range(0, 3);
+            // instantiate the group
+            currentGroup = Instantiate(group, displayPoint.position, UnityEngine.Quaternion.identity);
 
-            currentObject = Instantiate(objectsToChooseFrom[randomNumber], displayPoint.position, Quaternion.identity);
-            currentObject.transform.SetParent(displayPoint);
+            //Rotate on display point should work
+            currentGroup.transform.SetParent(displayPoint);
+
+            //check which group it is to fix x,y,z relative to parent
+            string groupName = group.name;
+            switch(groupName) {
+                case "RightArm":
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(95,-55, -29);
+                    break;
+                case "RightLeg": 
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(32,51,-42);
+                    break;
+                case "LeftLeg":
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(32, 51, -42);
+                    break;
+                case "Body":
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(-4, -22, 45);
+                    break;
+                case "Vertebra":
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(0, -17, -26);
+                    break;
+                case "Skull":
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(-2, -115, 111);
+                    currentGroup.transform.Rotate(new UnityEngine.Vector3(0,180,0));
+                    break;
+                case "RightHand": 
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(106, 6,-22);
+                    currentGroup.transform.Rotate(new UnityEngine.Vector3(-180,-1,3));
+                    break;
+                case "LeftArm":
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(-36, 84, 0);
+                    break;
+                case "LeftFoot":
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(-18, 54, -154);
+                    currentGroup.transform.Rotate(new UnityEngine.Vector3(64,-113,70));
+                    break;
+                case "LeftHand":
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(-21, -133, 11);
+                    currentGroup.transform.Rotate(new UnityEngine.Vector3(156,-2,-10));
+                    break;
+                case "RightFoot":
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(-64, -50, -154);
+                    currentGroup.transform.Rotate(new UnityEngine.Vector3(59,65,-116));
+                    break;
+                case "Rib":
+                    currentGroup.transform.localPosition = new UnityEngine.Vector3(13, -128, 924);
+                    break;
+ 
+                
+            }
+            //TODO: Highlight target bone
+            Transform boneToHighlight = currentGroup.transform.GetChild(boneIndex);
+            Renderer boneRenderer = boneToHighlight.GetComponent<Renderer>();
+            
             // Set UI layer
-            currentObject.layer = layerUI;
-            foreach (Transform child in currentObject.GetComponentsInChildren<Transform>(true))  
+            currentGroup.layer = layerUI;
+            foreach (Transform child in currentGroup.GetComponentsInChildren<Transform>(true))  
             {
                child.gameObject.layer = layerUI;
             }
-            // Set position
-            currentObject.transform.localPosition = new Vector3(6.4f, 58.6f, -784f);
-            // Set rotation
-            // currentObject.transform.rotation = new Vector3(43.296f, 6.468f, 0.395f);
-            // Set scale
-            currentObject.transform.localScale = new Vector3(0.5365187f, 0.5365187f, 0.51f);
-        }        
+
+        }
+
+
+
+
+
+        //currentGroup = Instantiate(prefabGroups[0], displayPoint.localPosition, Quaternion.identity);
+
+        //currentGroup.transform.locallocalPosition = new Vector3(0, 0, 0);
+        //currentGroup.transform.SetParent(displayPoint);
+
+        // Set localPosition
+        //currentGroup.transform.locallocalPosition = new Vector3(0, 0, 0);
+        // Set rotation
+        //currentGroup.transform.rotation = new Vector3(43.296f, 6.468f, 0.395f);
+        // Set scale
+        //currentGroup.transform.localScale = new Vector3(0.5365187f, 0.5365187f, 0.51f);
     }
+
+    // private void SpawnBone()
 }
--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,11 +4,43 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-    // public Vector3 rotateAmount;
-    public float rotationSpeed = 100f;
+    private bool isDragging = false;
+    private Vector3 lastMousePosition;
+
+    public float rotationSpeed = 10f;  // Adjust the speed of rotation
 
     void Update()
     {
-        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime, Space.Self);
+        // Detect mouse click and hold
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+            lastMousePosition = Input.mousePosition;
+        }
+
+        // Detect mouse release
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        // Rotate object when dragging
+        if (isDragging)
+        {
+            Vector3 delta = Input.mousePosition - lastMousePosition;
+
+            // Rotation on the X-axis (Vertical mouse drag)
+            float rotationX = delta.y * rotationSpeed * Time.deltaTime;
+
+            // Rotation on the Y-axis (Horizontal mouse drag)
+            float rotationY = -delta.x * rotationSpeed * Time.deltaTime;
+
+            // Apply rotation
+            transform.Rotate(Vector3.up, rotationY, Space.World);
+            transform.Rotate(Vector3.right, rotationX, Space.World);
+
+            // Update last mouse position
+            lastMousePosition = Input.mousePosition;
+        }
     }
 }
